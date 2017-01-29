27d74581452e0014ce41e72587ae804f664adb43
Amin Paydar <mapsgp920@gmail.com>
2017-01-29 20:43:09 +0330

    remove comments

--- src/main/java/ir/amin/schedule/entities/Resource.java
+++ src/main/java/ir/amin/schedule/entities/Resource.java
@@ -5,8 +5,6 @@ import ir.amin.schedule.trigger.Trigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Serializable;
-
 /**
  * Created by amin on 12/25/16.
  */
@@ -14,11 +12,9 @@ public class Resource implements Cloneable {
 
     private JobScheduler jobScheduler;
     private final Logger logger = LoggerFactory.getLogger(Resource.class);
-    private int time;
     private int speed;
     private boolean isFree = true;
     public Job allocatedJob;
-    private int rr = 2;
     private Trigger<Job> triggerObject;
 
     public Resource(Trigger triggerObject, int speed) {
@@ -54,7 +50,6 @@ public class Resource implements Cloneable {
     }
 
     public void allocate(Job j) {
-        //logger.info("Job {} reAllocate with Resourse with speed {}", j, speed);
         this.setFree(false);
         this.allocatedJob = j;
     }
@@ -74,94 +69,12 @@ public class Resource implements Cloneable {
         return triggerObject.trigger(this, time);
     }
 
-    /*public Job trigger(int time) {
-        this.time = time;
-        if (allocatedJob != null) {
-            // ToDo update remaning time
-            allocatedJob.setRemainingTime(allocatedJob.getReturnedTime() - 1);
-            // if (allocatedJob.getRemainingTime() <= 0)
-            if (time - allocatedJob.getStartTime() >= mapBurstTime(allocatedJob.getBurstTime())) {
-                allocatedJob.setReturnedTime(time);
-                setFree(true);
-                logger.info("\nJob {} \narrival at {}\nstart at {}\nburst time {}\nreturned time {}\n"
-                        , allocatedJob
-                        , allocatedJob.getArrivalTime()
-                        , allocatedJob.getStartTime()
-                        , mapBurstTime(allocatedJob.getBurstTime())
-                        , allocatedJob.getReturnedTime());
-                allocatedJob = null;
-                return allocatedJob;
-            }
-        }
-        return null;
-    }
-
-    public Job trigger_srtf(int time) {
-        this.time = time;
-        if (allocatedJob != null) {
-            // ToDo update remaning time
-            allocatedJob.setRemainingTime(allocatedJob.getReturnedTime() - 1);
-            // if (allocatedJob.getRemainingTime() <= 0)
-            if (time - allocatedJob.getStartTime() >= mapBurstTime(allocatedJob.getBurstTime())) {
-                allocatedJob.setReturnedTime(time);
-                setFree(true);
-                logger.info("\nJob {} \narrival at {}\nstart at {}\nburst time {}\nreturned time {}\n"
-                        , allocatedJob
-                        , allocatedJob.getArrivalTime()
-                        , allocatedJob.getStartTime()
-                        , mapBurstTime(allocatedJob.getBurstTime())
-                        , allocatedJob.getReturnedTime());
-                allocatedJob = null;
-                return allocatedJob;
-            }
-        }
-        return null;
-    }
-
-
-    public Job trigger_rr(int time) {
-        this.time = time;
-        if (allocatedJob != null) {
-            // ToDo update remaning time
-            allocatedJob.setRemainingTime(allocatedJob.getRemainingTime() - 1);
-            logger.info("\nJob {} \narrival at {}\nstart at {}\nburst time {}\nremaining time {}\n"
-                    , allocatedJob
-                    , allocatedJob.getArrivalTime()
-                    , allocatedJob.getStartTime()
-                    , allocatedJob.getBurstTime()
-                    , allocatedJob.getRemainingTime());
-            if (time - allocatedJob.lastExecTime >= rr) {
-                setFree(true);
-                logger.info("\nJob {} back to ready queue"
-                        , allocatedJob);
-                jobScheduler.backJob(allocatedJob);
-            } else if (allocatedJob.getRemainingTime() <= 0) {
-                //if (time - allocatedJob.getRemainingTime() >= mapBurstTime(allocatedJob.getBurstTime())) {
-                allocatedJob.setReturnedTime(time);
-                setFree(true);
-                logger.info("\nJob {} \narrival at {}\nstart at {}\nburst time {}\nreturned time {}\n"
-                        , allocatedJob
-                        , allocatedJob.getArrivalTime()
-                        , allocatedJob.getStartTime()
-                        , allocatedJob.getBurstTime()
-                        , allocatedJob.getReturnedTime());
-                allocatedJob = null;
-                return allocatedJob;
-            }
-        }
-        return null;
-    }*/
-
     public void setTriggerObject(Trigger<Job> triggerObject) {
         this.triggerObject = triggerObject;
     }
 
     public Job getAllocatedJob() {
         return allocatedJob;
-    }
-
-    public int getTime() {
-        return time;
     }
 
     public int mapBurstTime(int burstTime) {
@@ -177,7 +90,7 @@ public class Resource implements Cloneable {
 
     @Override
     public String toString() {
-        String str = "Resourse speed " + speed + "\n";
+        String str = "Resource speed " + speed + "\n";
         return str;
     }
 
